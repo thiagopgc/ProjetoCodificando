4a79166f1a1566bb9decd575e706bae475c4fcde
thiagopgc <thiagopereiragc@yahoo.com.br>
2021-12-05 11:53:06 -0300

    criado os passos no html e feito a ligação com o js.

--- cesar.js
+++ cesar.js
@@ -1,2 +1,8 @@
-var opcao = document.querySelector(".selacao");
-console.log(opcao);
+var passo = document.querySelector("#passo");
+
+escolha = document.addEventListener("click", function () {
+  var selecao = document.querySelector("#selecao").value; //value puxa o texto do seletor (#selecao).
+  if (selecao === "Cifra de Cesar") {
+    passo.style.display = "flex";
+  }
+});
