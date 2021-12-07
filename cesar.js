3cd03880b7bf66f9f67724c9fc5b128a50f008f1
thiagopgc <thiagopereiragc@yahoo.com.br>
2021-12-06 23:31:42 -0300

    criado uma tag button para codificar.

--- cesar.js
+++ cesar.js
@@ -1,8 +1,34 @@
 var passo = document.querySelector("#passo");
+var textoInicial = document.querySelector(".textoInicial");
+var textoRetorno = document.querySelector(".textoRetorno");
+var radio = document.querySelectorAll(".radio");
+var botao = document.querySelector(".botao");
 
 escolha = document.addEventListener("click", function () {
   var selecao = document.querySelector("#selecao").value; //value puxa o texto do seletor (#selecao).
-  if (selecao === "Cifra de Cesar") {
+  if (selecao == "cesar") {
     passo.style.display = "flex";
   }
 });
+
+// var alfabetoMinusculo = "abcdefghijklmnopqrstuvwxyz";
+// var alfabetoMaiusculo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var indice = 9;
+
+// var letraMinuscula = alfabetoMinusculo[indice];
+// var letraMaiuscula = alfabetoMaiusculo[indice];
+
+var textoInicial = +document.querySelector(".textoInicial").innerHTML;
+function voltar() {
+  if (textoInicial > 1) {
+    textoInicial--;
+    document.querySelector(".textoInicial").innerHTML = textoInicial;
+  }
+}
+
+function avancar() {
+  if (textoInicial < 25) {
+    textoInicial++;
+    document.querySelector(".textoInicial").innerHTML = textoInicial;
+  }
+}
